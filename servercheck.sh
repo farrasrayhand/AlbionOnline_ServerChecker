98ebbfbd4130e713e566e56b5d41899f2e1c1643
farrasrayhand <35839373+farrasrayhand@users.noreply.github.com>
2018-11-29 13:37:47 +0800

    Update servercheck.sh

--- servercheck.sh
+++ servercheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-clear
 mytitle="Albion Online Status"
 echo -e '\033]2;'$mytitle'\007'
+clear
 while true; do sleep 1; curl serverstatus.albiononline.com:9099; echo -e "\n" $(date)
 sleep 5s
 done

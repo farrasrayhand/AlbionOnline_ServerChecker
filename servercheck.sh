c0c54958772742b71b86d6454c685259fce5641b
farrasrayhand <35839373+farrasrayhand@users.noreply.github.com>
2018-11-29 15:33:43 +0800

    Update servercheck.sh

--- servercheck.sh
+++ servercheck.sh
@@ -2,10 +2,13 @@
 mytitle="Albion Online Status"
 echo -e '\033]2;'$mytitle'\007'
 clear
-while true; do echo -e "-Albion Url" && curl serverstatus.albiononline.com:9099 && echo -e "\n\n-Maintenance Url" && curl http://live.albiononline.com/status.txt && echo -e "\n"
-COLUMNS=$(tput cols) 
-title="$(date)" 
-printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+while true;
+do
+COLUMNS=$(tput cols)
+title="$(date)"
+printf "%*s" $(((${#title}+$COLUMNS) /2)) "$title"
+echo -e "\n"
+echo -e "-Albion Url" && curl serverstatus.albiononline.com:9099 && echo -e "\n\n-Maintenance Url" && curl http://live.albiononline.com/status.txt
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 sleep 5s
 done
